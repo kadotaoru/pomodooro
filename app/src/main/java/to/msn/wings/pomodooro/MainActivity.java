cff910cb87d7a7b11b78d94facc8dacdefd1c1ca
kadotaoru <daiyaogata898@gmail.com>
2021-02-14 23:37:41 +0900

    Initial commit

---
+++ app/src/main/java/to/msn/wings/pomodooro/MainActivity.java
@@ -0,0 +1,79 @@
+package to.msn.wings.pomodooro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView timerText;
+    private SimpleDateFormat dataFormat =
+            new SimpleDateFormat("mm:ss", Locale.US);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(android.R.id.content).setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        long countNumber = 1500000;   //25min
+        long interval = 10;
+
+        Button startButton = findViewById(R.id.start_button);
+        Button stopButton = findViewById(R.id.stop_button);
+
+        timerText = findViewById(R.id.timer);
+        timerText.setText(dataFormat.format(1500000));
+
+        final CountDown countDown = new CountDown(countNumber, interval);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start
+                countDown.start();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // stop
+                countDown.cancel();
+                timerText.setText(dataFormat.format(1500000));
+            }
+        });
+    }
+
+    class CountDown extends CountDownTimer{
+
+        CountDown(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            timerText.setText(dataFormat.format(millisUntilFinished));
+        }
+
+        @Override
+        public void onFinish() {
+            timerText.setText(dataFormat.format(1500000));
+        }
+    }
+}
