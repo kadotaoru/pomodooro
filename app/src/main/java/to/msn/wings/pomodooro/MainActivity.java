fe5f58bb275602abc47ff2c71f9403c572eb5adf
kadotaoru <daiyaogata898@gmail.com>
2021-02-15 11:41:31 +0900

    reset cord

--- app/src/main/java/to/msn/wings/pomodooro/MainActivity.java
+++ app/src/main/java/to/msn/wings/pomodooro/MainActivity.java
@@ -2,9 +2,11 @@ package to.msn.wings.pomodooro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.sip.SipSession;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,13 +15,18 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
-    //made branch!
-    private TextView timerText;
-    private SimpleDateFormat dataFormat =
-            new SimpleDateFormat("mm:ss", Locale.US);
+    private int breakTime = 300;
+    private int workTime = 1500;
+    private int current = workTime;
+    private boolean isWorkTime = false;
+    private boolean isStart = true;
+
+    private Timer timer;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,51 +40,11 @@ public class MainActivity extends AppCompatActivity {
             window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
-        long countNumber = 5000;   //25min   5sec
-        long interval = 10;
-
+        TextView textTitle = findViewById(R.id.textTitle);
+        TextView timerText = findViewById(R.id.timer);
         Button startButton = findViewById(R.id.start_button);
         Button stopButton = findViewById(R.id.stop_button);
+//        SipSession.Listener listener = StartStopListener();
 
-        timerText = findViewById(R.id.timer);
-        timerText.setText(dataFormat.format(5000));
-
-        final CountDown countDown = new CountDown(countNumber, interval);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // start
-                countDown.start();
-            }
-        });
-
-        stopButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                // stop
-                countDown.cancel();
-                timerText.setText(dataFormat.format(5000));
-            }
-        });
-    }
-
-    class CountDown extends CountDownTimer{
-
-        CountDown(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-            timerText.setText(dataFormat.format(millisUntilFinished));
-        }
-
-        @Override
-        public void onFinish() {
-            timerText.setText(dataFormat.format(5000));
-            CountDown.this.start();  //restart
-            CountDown.this.cancel();
-        }
     }
 }
